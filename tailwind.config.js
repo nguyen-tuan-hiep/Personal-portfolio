492ba46bcb6bd5c7638e84bed0c8689507366a5e
Nguyen Tuan Hiep <hiep454546@gmail.com>
2024-06-20 22:15:38 +0700

    Revert to previous commit <commit-hash>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				grayDark: "rgb(212 212 212)",
+				grayLight: "rgb(64 64 64)",
+			},
+			boxShadow: {
+				"custom": "0 2px 6px rgba(0, 0, 0, 0.7)",
+			},
+		},
 	},
 	plugins: [],
 };
